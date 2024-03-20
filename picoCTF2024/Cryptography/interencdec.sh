e5c09e999d495624e7e8d5306d6147e83de5761a
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-03-20 11:37:34 -0300

    Add files via upload

---
+++ picoCTF2024/Cryptography/interencdec.sh
@@ -0,0 +1,27 @@
+Description
+Can you get the real meaning from this file.
+Download the file
+
+cat enc_flag | base64 -d
+b'd3BqdkpBTXtqaGx6aHlfazNqeTl3YTNrXzg2a2wzMmsyfQ=='
+
+echo 'd3BqdkpBTXtqaGx6aHlfazNqeTl3YTNrXzg2a2wzMmsyfQ==' | base64 -d
+
+wpjvJAM{jhlzhy_k3jy9wa3k_86kl32k2}
+
+
+
+
+
+echo "wpjvJAM{jhlzhy_k3jy9wa3k_86kl32k2}" | tr [a-zA-Z] [t-za-s-T-ZA-S]
+#19 pulos
+
+picoCTF{t1m3m@ch1n3_e8c98b3a}
+
+W -> P (19)
+P -> I (19)
+J -> C (19)
+
+picoB-E{caesar_d3cr9pt3d_86de32d2}
+# Arrumando..
+picoCTF{caesar_d3cr9pt3d_86de32d2}
