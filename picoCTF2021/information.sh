a330a9ce77351f2ca98e8350b9259ae09139da98
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-03-25 20:04:24 -0300

    Create information.sh

---
+++ picoCTF2021/information.sh
@@ -0,0 +1,17 @@
+Description
+Files can always be changed in a secret way. Can you find the flag? 
+
+wget https://mercury.picoctf.net/static/a614a27d4cb251d04c7d2f3f3f76a965/cat.jpg
+
+W5M0MpCehiHzreSzNTczkc9d
+cGljb0NURnt0aGVfbTN0YWRhdGFfMXNfbW9kaWZpZWR9
+
+
+"cGljb0NURnt0aGVfbTN0YWRhdGFfMXNfbW9kaWZpZWR9" | base64 -d
+picoCTF{the_m3tadata_1s_modified}
+
+
+echo "W5M0MpCehiHzreSzNTczkc9d" | base64 -d
+[42!573]
+
+picoCTF{the_m3tadata_1s_modified}
