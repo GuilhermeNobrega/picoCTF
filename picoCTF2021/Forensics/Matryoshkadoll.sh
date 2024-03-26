406325c4b1f6f9330aef091db8f28929aa1dfe44
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-03-25 21:51:15 -0300

    Create Matryoshkadoll.sh

---
+++ picoCTF2021/Forensics/Matryoshkadoll.sh
@@ -0,0 +1,11 @@
+Matryoshka dolls are a set of wooden dolls of decreasing size placed one inside another. What's the final one? Image: this
+https://mercury.picoctf.net/static/f6cc2560a70b1ea811c151accba5390f/dolls.jpg
+
+wget https://mercury.picoctf.net/static/f6cc2560a70b1ea811c151accba5390f/dolls.jpg
+
+
+string 
+binwalk
+foremost
+exitfool
+(Estudar isso para resolver o ctf)
