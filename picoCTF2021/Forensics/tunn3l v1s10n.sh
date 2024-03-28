6ac1500271a89c7f59099f2a3bb3263374fc9fed
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-03-28 20:43:26 -0300

    Create tunn3l v1s10n.sh

---
+++ picoCTF2021/Forensics/tunn3l v1s10n.sh
@@ -0,0 +1,9 @@
+We found this file. Recover the flag.
+
+https://mercury.picoctf.net/static/06a5e4ab22ba52cd66a038d51a6cc07b/tunn3l_v1s10n
+
+wget https://mercury.picoctf.net/static/06a5e4ab22ba52cd66a038d51a6cc07b/tunn3l_v1s10n
+
+file tunn3l_v1s10n
+
+
